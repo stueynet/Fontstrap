a4ea9ee412ebfcab4c6760f8e7384978e8363af2
Stuart Starr <stuart@stueynet.com>
2012-08-27 20:48:09 -0400

    Revert "Incorporated caret fix in responsive dropdown"
    This reverts commit afc97d0e0a2f13d5813cb167476be21a6b738606.

--- _header.php
+++ _header.php
@@ -12,14 +12,14 @@ require_once('config/config.php');
 	<meta name="author" content="">
 
 	<!-- Le styles -->
-	<link href="fontstrap/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="fontstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<style type="text/css">
 		body {
 			padding-top: 60px;
 			padding-bottom: 40px;
 		}
     </style>
-	<link href="<?php echo $site_url; ?>/fontstrap/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="<?php echo $site_url; ?>/fontstrap/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link href="<?php echo $site_url; ?>/fontstrap/fontawesome/css/font-awesome.css" rel="stylesheet">
 	<link href="<?php echo $site_url; ?>/assets/css/theme.css" rel="stylesheet">
 
